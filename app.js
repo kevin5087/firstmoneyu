6aa3a49013d1e0198d200cd511881de5c33619c2
kevin5087 <41320019+kevin5087@users.noreply.github.com>
2018-08-01 14:56:16 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express(); //建立一個Express伺服器
+const https = require('https');
+let name;
+
+https.get('https://api.coinmarketcap.com/v2/ticker/', (resp) => {
+  let data = '';  
+// A chunk of data has been recieved.
+resp.on('data', (chunk) => {
+    data += chunk;
+  });
+ 
+  // The whole response has been received. Print out the result.
+resp.on('end', () => {
+    //console.log( JSON.parse(data));
+    name = JSON.parse(data);
+   
+  });
+ 
+}).on("error", (err) => {
+    console.log("Error: " + err.message);
+
+});
+
+app.get("/rank/:id", function(req, res) {
+    // console.log('xxxxxxxxx', name);
+    const test = Object.values(name.data);
+    const id = req.params.id; 
+    console.log('id', Number(id));
+    const mater = test.filter( number => number.rank === Number(id));
+    console.log('aa', mater);
+
+    //console.log('aaereer', mater);
+    //res.json(mater)
+       
+});
+
+     
+  
+
+
+
+
+  app.listen(3000, function () {
+    console.log('Example app is running on port 3000!');}
+  );
