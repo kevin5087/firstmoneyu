6f1090865e71920c2b628d84c109560e1b33182a
kevin5087 <41320019+kevin5087@users.noreply.github.com>
2018-08-06 14:28:22 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+"use strict";
+
+const express = require('express');
+const app = express(); //建立一個Express伺服器
+const https = require('https');
+const bodyParser = require("body-parser");
+let name;
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.use(bodyParser.json());
+
+
+https.get('https://api.coinmarketcap.com/v2/ticker/', (resp) => {
+  let data = '';  
+// A chunk of data has been recieved.
+resp.on('data', (chunk) => {
+    data += chunk;
+  });
+ 
+  // The whole response has been received. Print out the result.
+resp.on('end', () => {
+    //console.log( JSON.parse(data));
+    name = JSON.parse(data);
+   
+  });
+ 
+}).on("error", (err) => {
+    console.log("Error: " + err.message);
+
+});
+
+app.post("/rank/:id", function(req, res) {
+ 
+    const test = Object.values(name.data);
+    //const id = req.params.id; 
+    console.log('id', Number(id));
+    const mater = test.filter( number => number.rank === Number(id));
+    console.log('aa', mater);  
+    console.log('aaaaa', req.params.id);
+    return res.json({
+      id: req.params.id
+    });
+
+    var speech = 
+    req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.echoText
+      ? mater
+      : "Seems like some problem. Speak again.";
+    
+    return res.json({
+      speech: speech,
+      displayText: mater,
+      source: "webhook-echo-sample"
+    });
+
+});
+
+
+app.listen(process.env.PORT || 8000, function() {
+  console.log("Server up and listening");
+});
+
+
+ 
