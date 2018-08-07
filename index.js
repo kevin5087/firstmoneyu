d01d57ef3df07d621015004eaba705267e978a97
kevin5087 <41320019+kevin5087@users.noreply.github.com>
2018-08-07 15:33:03 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ app.post("/rank", function(req, res) {
     //console.log(typeof mater);
     //console.log('asdff', JSON.stringify(mater));
     const mma = mater[0].name;
+    const money = mater[0].quotes[0].USD[0].price;
     //const max = d
     //console.log('aaaaa', mma);
     
@@ -57,7 +58,7 @@ app.post("/rank", function(req, res) {
     req.body.result &&
     req.body.result.parameters &&
     req.body.result.parameters.echoText
-      ? `${mma}:的本日最高是:`
+      ? `${mma}:的本日價格是:${money}}`
       : "Seems like some problem. Speak again.";
     //console.log(typeof mma);
 
